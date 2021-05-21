94f500559e44ac3c002e9361b636f0dd1f42cc6b
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2021-05-21 15:58:38 +0100

    TMP

--- terraform/log-subscription.tf
+++ terraform/log-subscription.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_log_subscription_filter" "central_log" {
-  for_each = toset(
-    trimspace(var.log_subscription_destination_arn) != "" ? [
-      for k, lg in aws_cloudwatch_log_group.lambdas: lg.name
-    ] : []
+  for_each = (
+    trimspace(var.log_subscription_destination_arn) != "" ? {
+      for k, lg in aws_cloudwatch_log_group.lambdas: k => lg.name
+    } : {}
   )
 
-  name           = "central_log"
-  log_group_name = "/aws/lambda/${each.key}"
+  name           = "central_log_${each.key}"
+  log_group_name = each.value
   filter_pattern = ""
 
   destination_arn = var.log_subscription_destination_arn
